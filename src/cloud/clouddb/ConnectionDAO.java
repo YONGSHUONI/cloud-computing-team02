a43fe7709ee3da42a34fa5af3f9e0ca5ef334b34
Shuo Zhou <sz144@outlook.com>
2017-04-25 20:03:55 +0100

    Update ConnectionDAO.java

--- src/cloud/clouddb/ConnectionDAO.java
+++ src/cloud/clouddb/ConnectionDAO.java
@@ -1,40 +1,28 @@
 package cloud.clouddb;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+import java.util.*;
 
-/**
- * Servlet implementation class ConnectionDAO
- */
-@WebServlet("/ConnectionDAO")
-public class ConnectionDAO extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-    /**
-     * Default constructor. 
-     */
-    public ConnectionDAO() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+public class ConnectionDAO {
+	// create connection
+	public Connection getConnection(){
+		String connectionUrl = "jdbc:mysql://127.0.0.1:3306/SENG_DB";
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			//DriverManager.registerDriver(new com.mysql.jdbc.Driver ());
+			connection = DriverManager.getConnection(connectionUrl, "root", null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		return connection;
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	
+	public void closeConnection(Connection connection){
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
